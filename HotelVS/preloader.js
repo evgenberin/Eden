853e756118b3948987389c5af35715ad65862fd1
evgenberin <bakerandel7731@proton.me>
2025-08-06 10:03:31 +0700

    Delete HotelVS directory

--- HotelVS/preloader.js
+++
@@ -1,6 +0,0 @@
-// Tự động thêm class sau 3s để trượt preloader lên
-window.addEventListener('load', function () {
-  setTimeout(function () {
-    document.body.classList.add('page-loaded');
-  }, 2000);
-});
